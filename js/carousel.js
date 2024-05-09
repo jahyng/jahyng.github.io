784e81e1148f1d9d96e1869ce1e971a5774888b6
jahyng <97709079+jahyng@users.noreply.github.com>
2024-05-08 20:38:27 -0700

    refactored slide widths using for loop

--- js/carousel.js
+++ js/carousel.js
@@ -11,9 +11,9 @@ console.log(slidesWidth);
 
 
 //arrangethe slides next to one another
-slides[0].style.left = slidesWidth*0+'px';
-slides[1].style.left = slidesWidth + 'px';
-slides[2].style.left = slidesWidth *2 + 'px';
+for (let i = 0; i < slides.length; i++) {
+    slides[i].style.left = slidesWidth *i + 'px';
+}
 // when i click left move slies to the left
 // when i click right move slides to the right
 // when i click nav indicators move to slide of clicked indicator
