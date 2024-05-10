f76c6699fe0b62b7f9c0eb55edb5cf69d9023e09
jahyng <jwyang@ucsd.edu>
2024-05-09 17:44:30 -0700

    fixed transitions in JS and added funcitonality for both buttons

--- js/carousel.js
+++ js/carousel.js
@@ -1,7 +1,7 @@
 const track = document.querySelector('.carousel_track');
 const slides = Array.from(track.children);
 const nextButton = document.querySelector('.carousel_button--right');
-const prevButton = document.querySelector('.carousel_button--right');
+const prevButton = document.querySelector('.carousel_button--left');
 const dotsNav = document.querySelector('.carousel_nav');
 const dots = Array.from(dotsNav.children);
 
@@ -10,21 +10,39 @@ const slidesWidth = slides[0].getBoundingClientRect().width;
 console.log(slidesWidth);
 
 
-//arrange the slides next to one another
+// Arrange the slides next to one another
 const setSlidePosition = (slide, index) => {
     slide.style.left = slidesWidth * index + 'px';
 }
 slides.forEach(setSlidePosition);
 
-// TODO: when i click left move slies to the left
+const moveToSlide = (track, currentSlide, targetSlide) => {
+    track.style.transform = 'translateX(-' + targetSlide.style.left + ')';
+    currentSlide.classList.remove('current-slide');
+    targetSlide.classList.add('current-slide');
+    
+}
 
-// TODO: when i click right move slides to the right
+// When I click left move slies to the left
+prevButton.addEventListener('click', e => {
+    const currentSlide = track.querySelector('.current-slide');
+    const prevSlide = currentSlide.previousElementSibling;
+    
+    moveToSlide(track,currentSlide,prevSlide);
+});
+
+//When I click right move slides to the right
 nextButton.addEventListener('click', e => {
     const currentSlide = track.querySelector('.current-slide');
     const nextSlide = currentSlide.nextElementSibling;
-    const amountToMove = nextSlide.style.left;
-    //move to the next slide
-    track.style.transform = translateX(-' + amountToMove + ');
+    
+    moveToSlide(track,currentSlide,nextSlide);
 });
-// TODO: when i click nav indicators move to slide of clicked indicator
 
+// TODO: when i click nav indicators move to slide of clicked indicator
+dotsNav.addEventListener('click', e => {
+    // what indicator was clicked
+    const targetDot = e;
+
+    console.log(e.target);
+});
